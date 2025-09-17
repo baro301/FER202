3e5a3ca476e99941d61d120d215b0264f5a8c129
baro301 <phunggiabao810@gmail.com>
2025-09-17 10:42:12 +0700

    Delete slot3/ex1 directory

--- slot3/ex1/src/exercise1.js
+++
@@ -1,8 +0,0 @@
-const double = (x) => x * 2;
-console.log(double(4)); // 14
-
-const double2 = (x) => {return x * 2;};         
-console.log(double2(7)); // 14
-
-const isEven = (x) => {return x % 2 === 0;}
-console.log(isEven(7)); // false
