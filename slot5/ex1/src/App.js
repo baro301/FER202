d640c46d68e4c2a48877657d8987dcb076b330d4
baro301 <phunggiabao810@gmail.com>
2025-09-26 07:15:15 +0700

    last update

--- slot5/ex1/src/App.js
+++ slot5/ex1/src/App.js
@@ -8,11 +8,6 @@ function App() {
       <Exercise1 />
       <Exercise2 />
       <Exercise3 />
-      <Exercise4 />
-      <Exercise5 />
-      <Exercise6 />
-      <Exercise7 /> 
-      <Exercise8 />
     </div>
   );
 }
