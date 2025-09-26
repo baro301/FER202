767f1e24d9fc1303cfc43ec9f98030b066a3e8a3
baro301 <phunggiabao810@gmail.com>
2025-09-26 07:12:42 +0700

    update exercise2

--- slot5/ex1/src/App.js
+++ slot5/ex1/src/App.js
@@ -8,6 +8,11 @@ function App() {
       <Exercise1 />
       <Exercise2 />
       <Exercise3 />
+      <Exercise4 />
+      <Exercise5 />
+      <Exercise6 />
+      <Exercise7 /> 
+      <Exercise8 />
     </div>
   );
 }
